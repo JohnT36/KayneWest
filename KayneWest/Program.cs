9cabf1ce4036e9b154b4ec489d346acea191b6ce
John Tuck <j.tuck1336@gmail.com>
2022-06-16 07:32:40 -0400

    Add project files.

---
+++ KayneWest/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KayneWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+            QuoteGenerator.RonSwanson();
+            Console.ReadLine();
+            QuoteGenerator.KanyeWest();            
+            QuoteGenerator.RonSwanson();
+            Console.ReadLine();
+            QuoteGenerator.KanyeWest();
+            QuoteGenerator.RonSwanson();
+            Console.ReadLine();
+            QuoteGenerator.KanyeWest();
+            Console.ReadLine();
+            QuoteGenerator.KanyeWest();
+            Console.ReadLine();
+            QuoteGenerator.RonSwanson();            
+            QuoteGenerator.KanyeWest();
+            Console.ReadLine();
+            QuoteGenerator.RonSwanson();
+            QuoteGenerator.KanyeWest();
+            Console.ReadLine();
+            QuoteGenerator.RonSwanson();
+            QuoteGenerator.KanyeWest();
+            Console.ReadLine();
+            QuoteGenerator.RonSwanson();
+
+            // var client = new HttpClient();
+            // var kanyeURL = "https://api.kanye.rest";
+            // var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            // var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            // Console.WriteLine(kanyeQuote);
+        }
+    }
+}
---
+++ KayneWest/QuoteGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KayneWest
+{
+    
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+        public static void KanyeWest()
+        {
+
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+           
+            Console.WriteLine(" -----------------------------------------");
+            Console.WriteLine($"Kanye : {kanyeQuote}.");
+
+        }
+        public static void RonSwanson()
+        {
+
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;            
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                                                         //   ["I love nothing."]
+            
+            Console.WriteLine(" -----------------------------------------");
+            Console.WriteLine($"RON : {ronQuote}.");
+
+        }
+
+
+        
+
+       
+    }
+}
